e3f0b51587c96a1a00e81a82e7987665d048597d
sohfix <23560703+sohfix@users.noreply.github.com>
2024-08-22 21:14:32 -0500

    0.0.2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #! scripts/sysix_wrapper.sh
 
-#! /home/sohfix/programs/sysix/sys_ops/stream/bin/python3.11
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print('shit')
     pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- scripts/sysix_wrapper.sh
+++ scripts/sysix_wrapper.sh
@@ -2,8 +2,10 @@
 
 if [[ $(hostname) == "rog-g14" ]]; then
     /home/sohfix/programs/sysix/sys_ops/stream/bin/python3.11 "$@"
+    echo "worked"
 elif [[ $(hostname) == "sohfix-imac" ]]; then
-    /home/sohfix/programs/development/stream/bin/python3.12 "$@"
+    /home/sohfix/PycharmProjects/sys_ops/.sysix/bin/python3.11 "$@"
+    echo "worked"
 else
     echo "Unknown machine!"
     exit 1
