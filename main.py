662d4fc4c20b1ea9cfb416c39887c1144e941d04
sohfix <23560703+sohfix@users.noreply.github.com>
2024-08-22 20:54:24 -0500

    update 0.0.1

---
+++ main.py
@@ -0,0 +1,7 @@
+#! /home/sohfix/programs/sysix/sys_ops/stream/bin/python3.11
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
